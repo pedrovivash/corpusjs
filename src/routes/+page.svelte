f551ccabe34bfae1e3f7edac2e7115bbe0c4c131
pvh <pedrovivash@gmail.com>
2022-10-24 10:55:26 -0400

    feat: Added globalwordnet/english-wordnet and parsed with fxp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import fxp from "fast-xml-parser"
+    import { onMount } from 'svelte';
+
+    console.clear()
+
+    const parser = new fxp.XMLParser()
+    
+    onMount(async () => {
+        const response = await fetch('/wn.xml')
+        const text = await response.text()
+        const wordnet = parser.parse(text)
+        console.log(wordnet)
+    })
+
+    
+</script>
