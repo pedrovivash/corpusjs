672cb580e4e96a5f71723337557de1e3b57a1f5c
pvh <pedrovivash@gmail.com>
2022-10-26 05:35:40 -0400

    feat/style: Functions for WPM measurement for RSVP, member extraction, basic UI, async loading of content.
    There are 1.7 times more members than there are synsets. I'd rather the definitions to remain longer since they repeat for every 1.7 members or so. The UI: a progress bar, a space underneath it that says what POS.subgroup is being displayed, which I will be adding a menu button on top of that provides a chance to skip to another 'chapter,' the main display that prints the word in preserved case, the member count (and for my purposes the index, sort of,) and the definition. The page is 600x300, for readability, and responds fine to stretching and zooming. Surely more adjustments could be made to make it more readable. It requires a space for buttons/kb input instructions, jkl I think, and another for form input. I need to allow for searching. Though, not via url query. The display function fills ariables at a given WPM rate. Most likely I'll have to wrap it in another function that feeds it indeces, and place before it a function that takes a word, matches some, and returns possible indices to consider. Things I still want to explore is adding images, a way to suggest the next and previous word in the UI, as surrounding words have similarity in meaning, the site still needs a footer, a logo, share buttons, about modal, a way to store favorite words client side for sessions and bookmarking, and a loading dialogue that is more engaging. The latter will probably be like instrubtions, as the 100 MB wordnet takes a long time to download. Regarding that, maybe there is a server side way accesible to me for dealing with that. Perhaps by preloading the main array or batching it I can cut down on that.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,45 @@
 <script>
     import fxp from "fast-xml-parser"
     import { onMount } from 'svelte';
-	import { each, xlink_attr } from "svelte/internal";
+
 
     console.clear()
 
+    //missing attrbibuteNamePrefix: "@_"
     const options = {
         ignoreAttributes: false,
         ignorePiTags: true
     }
     const parser = new fxp.XMLParser(options)
 
-    let wordnetArray = []
+    function rsvpMeasure(word, wpm, avgLength = avgMemberLength) {
+        const millisecond = 1000
+        const avgEngWordLength = 5.2
+        const avgWordLength = avgLength
+        const chunkLength = word.length
+        const reserveTime = (1/((wpm*avgWordLength)/60))*chunkLength
+        return reserveTime*millisecond
+    }
+
+    let synsetDefines
+    let synsetsLength = 0
+    let sumMembersLengths = 0
+    let avgMemberLength = 0
+    let position = 0
+    let member = ''
+    let pos = ''
+    let definition = ''
+
+    async function displayDefines(defines, wpm = 500) {
+        let i = 1
+        for (const x of defines) {    
+            for (const y of x) {
+                i++
+                [position, member, pos, definition] = [i, y[0], y[1], y[2]]
+                await new Promise(r => setTimeout(r, rsvpMeasure(member, wpm)))
+            }
+        }
+    }
 
     onMount(async () => {
         
@@ -22,20 +50,20 @@
         const lexicals = wordnet.LexicalResource.Lexicon.LexicalEntry
         const synsets = wordnet.LexicalResource.Lexicon.Synset
         const syntax = wordnet.LexicalResource.Lexicon.SyntacticBehaviour
-        
+
         /*
         legend:
             aao: array of objects
             off: offset
             i: index
             gate: return value
+            wpm: words per minute (target)
+            avgLegnth: against what the word (param) was sourced from
         
         note: 
             * I don't have to say this to myself, but some of thease are to learn
             the LMF format.
         */
-
-    
 
         // validate
 
@@ -49,18 +77,22 @@
         }
         
         // extract nested keys
+        // gate params: see switch/case
         function getSynsetEntries(aoo, i, gate) {
             const synsetArray = flatSynset(aoo, i)
             let giveReturn = []
+            let toReturn = []
             let copy = ''
             let giveReturnValues = []
+            let giveReturnValue = ''
             let giveReturnKey = ''
             const keyConditions = ['Definition', 'SynsetRelation', 'Example', '@_', '#']
+            const defineConditions = ['Definition', '@_members', '@_lexfile']
             switch (gate) {
                 case 'keys':
                     synsetArray.filter(x => {                        
                         keyConditions.some(y => y == x ||  
-                        y == x.slice(0,2) || 
+                        y == x.slice(0, 2) || 
                         y == x[0]) ?
                         giveReturn.push(x) : 
                         null
@@ -69,7 +101,7 @@
                 case 'values':
                     synsetArray.filter(x => {
                         keyConditions.some(y => y == x || 
-                        y == x.slice(0,2) || 
+                        y == x.slice(0, 2) || 
                         y == x[0]) ?
                         null : 
                         giveReturn.push(x)
@@ -79,45 +111,192 @@
                 case 'entries':
                     synsetArray.filter(x => {
                         keyConditions.some(y => y == x ||  
-                        y == x.slice(0,2) || 
+                        y == x.slice(0, 2) || 
                         y == x[0]) ?
                         giveReturnKey = x : 
                         giveReturnValues.push(x) 
                         giveReturnKey != copy ?
                         [copy, giveReturnValues] = [giveReturnKey, []] :
-                        giveReturn.length == 0 || giveReturn[giveReturn.length - 1][0] != giveReturnKey ?
-                        giveReturn[giveReturn.length] = [giveReturnKey, giveReturnValues] :
+                        giveReturn.length == 0 || 
+                        giveReturn[giveReturn.length - 1][0] != giveReturnKey ?
+                        giveReturn[giveReturn.length] = 
+                        [giveReturnKey, giveReturnValues] :
                         null
                     })
                     break
+                case 'define':
+                    synsetArray.filter((x, i) => {
+                        defineConditions.some(y => y == x) ?
+                        giveReturnKey = x :
+                        synsetArray[i - 1] == giveReturnKey ?
+                        giveReturnValue = x :
+                        null
+                        giveReturnValue != '' ?
+                        [giveReturn[giveReturn.length], giveReturnValue] = 
+                        [[giveReturnKey, giveReturnValue], ''] :
+                        null
+                    })
             }
-            return giveReturn
+            
+            if (gate != 'define') {
+                toReturn = giveReturn
+            }
+            else {
+                const membersSplit = giveReturn[1][1].
+                replaceAll('-a-',"'").
+                split(" ").
+                map(x => x.slice(5, x.length - 2).replaceAll('_',' '))
+                membersSplit.forEach(x => {
+                    synsetsLength+=1
+                    sumMembersLengths += x.length
+                    toReturn.push([x,
+                    giveReturn[2][1],
+                    giveReturn[0][1]
+                    ])}
+                )
+                avgMemberLength = sumMembersLengths/synsetsLength
+            }
+            return toReturn
         }
         
         // see range of Sysnets
         function getDecaFunc(off = 0, aao = synsets, func = flatSynset, gate = 'entries') {
-            const range = [0,1,2,3,4,5,6,7,8,9]
+            const range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             return range.map(x => x + off).map(y => func(aao, y, gate))
         }
 
         // see range of Sysnets
         function getAllFunc(aao = synsets, func = flatSynset, gate = 'entries') {
-            const range = aao.map((x,i) => i)
+            const range = aao.map((x, i) => i)
             return range.map(y => func(aao, y, gate))
         }
 
 
         // function calls
-        //console.log(getDecaFunc(50, synsets, getSynsetEntries, 'keys'))
-        console.log(wordnetArray = getAllFunc(synsets, getSynsetEntries, 'values'))
-        //console.log(synsets[0])
-
+        console.log(getAllFunc(synsets, getSynsetEntries, 'keys'))
+        synsetDefines = getAllFunc(synsets, getSynsetEntries, 'define')
+        //console.log(wordnetArray = flatSynset(synsets,0))
+        
+        
     })
     
 </script>
 
-{#each wordnetArray as x}
-    <p>
-        {(x)}
-    </p>
-{/each}
+<div style='display:flex; justify-content:center'>
+    <main style='width:600px; height:300px; border:18px solid black; padding:18px; overflow:hidden'>
+        <noscript>This page requires JavaScript.</noscript>
+        {#if synsetDefines == undefined}    
+            <p style='text-align:center'>
+                Loading Synsets
+            </p>
+        {/if}
+        {#await displayDefines(synsetDefines,1000)}
+            <small>{pos}</small>
+            <br/>
+            <progress class=this-prog style='position:absolute; top:36px;' value={position} max={synsetsLength} />
+            <h1 style='height:2rem;text-transform:none'>
+                {member}
+            </h1>
+            <p style='text-align:right'>
+                <small>{position}</small>
+            </p>
+            <p>
+                {definition}
+            </p>
+
+        {/await}
+    </main>
+</div>
+
+<style>
+    :global(*) {
+        -ms-overflow-style: none; /* for Internet Explorer, Edge */
+        scrollbar-width: none; /* for Firefox */
+    }
+
+    :global(*::-webkit-scrollbar) {
+        display: none; /* for Chrome, Safari, and Opera */
+    }
+
+    progress {
+        /* Reset the default appearance */
+        -webkit-appearance: none;
+        -moz-appearance:none;
+        appearance: none
+    }
+    .this-prog {
+        background-color:#dddddd;
+        border: none;
+        min-width: 1rem;
+        max-width: 10%;
+        height: 9px;
+    }
+    .this-prog::-webkit-progress-bar {
+        background-color:#dddddd
+    }
+    .this-prog[value]::-moz-progress-bar {
+        background-color: #80d000
+    }
+    .this-prog[value]::-webkit-progress-value {
+        background-color: #80d000
+    }
+
+    h1 {
+        color: #808080;
+        font-family: 'Arial';
+        font-size: 32px;
+        letter-spacing: .05em;
+        line-height: 1.5rem;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+    h2 {
+        color: #808080;
+        font-family: 'Arial';
+        font-size: 20px;
+        letter-spacing: .05em;
+        line-height: 1.5em;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+    p {
+        color: #808080;
+        font-family: 'Arial';
+        font-size: 18px;
+        letter-spacing: .11em;
+        line-height: 1.5em;
+        font-weight: normal;
+    }
+    small {
+        color: #bbbbbb;
+        font-family: 'Arial';
+        font-size: 10px;
+        letter-spacing: .05em;
+        line-height: 1.25em;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+    br {
+        line-height: 24px;
+    }
+    hr {
+        border: 9px solid black;
+    }
+    a {
+        text-decoration: none;
+    }
+    a:link {
+        color: #80d000
+    }
+    a:hover {
+        background-color: black;
+        color: white !important;
+    }
+    a:visited {
+        color: black
+        
+    }
+    a:active {
+        color: #c5f17e !important;
+    }
+</style>
