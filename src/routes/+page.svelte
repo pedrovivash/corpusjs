dbc84b1c5f1f421bb0b57aa4aafbc808dbf1e3c7
pvh <pedrovivash@gmail.com>
2022-10-31 09:34:49 -0400

    style//test: Changing loader to svg cycle from gif. Added hilites to buttons and fixed positions for controls.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -109,8 +109,10 @@
     // other
     let delay
     let stopCheck
+    let loaderIndex = 1
 
-
+    const loaderRange = Array(65).fill('x',0,65).map( (x, i )=> [i+1,(i+1).toString().padStart(2, '0')])
+    
     // serves the data
     /* BUGS
     async function displayAllDefines(definesProc, wpmReq, positionReq, reverseReq) {
@@ -151,7 +153,8 @@
         displaySearch = 'initial'
     }
     function searchDisplayOff(){
-        isSearch ?
+        console.log(isResults)
+        isResults ?
         null :
         displaySearch = 'none'
     }
@@ -163,8 +166,8 @@
    
     // delay handling in async
     function createDelay(intrpt = desiredWPM) {
-        intrpt == 0 ?
-        delay =  new Promise(r => setTimeout(r, rsvpMeasure(member, intrpt))):
+        //intrpt == 0 ?
+        //delay =  new Promise(r => setTimeout(r, rsvpMeasure(member, intrpt))):
         delay =  new Promise(r => setTimeout(r, rsvpMeasure(member, intrpt)))
         
     }
@@ -207,10 +210,12 @@
             stopCheck.checked = false
             desiredPosition = sindexInt
             displaySearch = 'none'
+            
         }
         else {
             desiredPosition = sindexInt
             displaySearch = 'none'
+            
         }
     }
 
@@ -294,7 +299,20 @@
         
     }
     
-    
+    // loader animate
+
+    async function animateLoader(frameIn) {    
+
+        cancelDelay()
+        createDelay(60)
+        await delay
+        let frame = frameIn
+        frame == 2 ?
+        frame = 1 :
+        frame++
+        loaderIndex = frame
+
+    }
 
     // search members
     async function searchTermReturn(term, aoaoa, synsetsArray){
@@ -325,6 +343,10 @@
         })
 
         const giveReturn = searchResFormat.map((x, i) => x.concat(realIDs[i].sort((a, b)=> a - b).toString().replaceAll(',',' | '))) 
+        giveReturn.length > 0 ?
+        isResults = true :
+        isResults = false
+        
         return giveReturn.map(x => [x[1],x[3].split(' | ')])
     }
     
@@ -533,11 +555,35 @@
 
 <div style='display:flex; justify-content:center'>
     <main style='width:600px; height:auto; border:18px solid black; padding:18px; overflow:hidden'>
+            <div style='width=100%;height:128px;background-color:green'>
+                <div style='display:flex; width:100%; height:128px;'>
+               
+                <div style='width:204px; min-width: 0px'></div>
+                
+                <div>
+                {#each loaderRange as frameIndex}
+                    <img class='loading-img' id='loading-frame-{frameIndex[0]}' style='' src='rsvp-synsets-loader/rsvp-synsets-loader_{frameIndex[1]}.svg' alt='animation of letters per frame at 60hz'>
+                {/each}
+                </div>
+
+                <div style='min-width:204px;'></div>
+
+            </div>   
+             </div>
+            <div style='display:flex;flex-direction:column;align-items:center'>   
+                <!--
+                {#await animateLoader(loaderIndex)}
+                    <img class='loading-img' style='height:25%;width:25%' src='rsvp-synsets-loader/test-loader ({loaderIndex}).svg' alt='animation of letters per frame at 60hz'>
+                {/await}
+                --> 
+ 
+                <p class='loading' style='text-align:center'>
+                    Loading Synsets
+                </p>
+            </div>
         <noscript>This page requires JavaScript.</noscript>
-        {#if synsetDefines == undefined}    
-            <p style='text-align:center'>
-                Loading Synsets
-            </p>
+        {#if synsetDefines == undefined}                
+
         {/if}
         {#await gatherUserInput(position, synsetDefines)}
             <div style='cursor:pointer;display:flex;flex-direction: row;justify-content: flex-start;align-items:center'>
@@ -563,7 +609,7 @@
                         <input type='button' class='step-button' name='STEPUP' on:click={()=> stepPosition('up')} value='STEP +1'>
                     </div>    
                     <button class='dropbtn' on:click={()=> chaptersDisplay()} name='partofSpeech'>CHAPTERS<div class='arrow' /></button>
-                    <div class='dropdown-container' style='display:{displayChapter}'>
+                    <div class='dropdown-container' on:mouseleave={()=> displayChapter = 'none'} style='display:{displayChapter}'>
                         {#each lexPartArray as lexPart}
                             <button class='dropdown-content' style='' on:click={()=> chapterSwitch(lexPart[0])}>{lexPart[1]}</button>
                         {/each}
@@ -576,10 +622,11 @@
                     </div>
                     <div style='display=flex; flex-direction:row; justify-content:space-between; width: 148px; align-items:center'>
                         <MagnifyingGlass style='top:3px; position:relative; color:#bbbbbb' />
-                        <input type='search' class='search-input' placeholder='>>' on:focus={()=> searchDisplayOn()} on:blur={()=> searchDisplayOff()} bind:value={searchTerm} name='SEARCH'>
+                        <input type='search' class='search-input' autocomplete='off' placeholder='>>' on:focus={()=> searchDisplayOn()} on:blur={()=> searchDisplayOff()} bind:value={searchTerm} name='SEARCH'>
                     </div>
-                    <div class='search-results-container' style='display:{displaySearch}'>
+                    <div class='search-results-container' on:mouseleave={()=> displaySearch = 'none'} style='display:{displaySearch}'>
                         {#await searchTermReturn(searchTerm, lexicalSearchTerms, synsetDefines) then result}
+                            {#if result.length > 0} 
                                 {#each result as searchDisp}
                                     <label for='memberIndex'>{searchDisp[0]}</label>
                                     <div name='memberIndex' class='member-index'>
@@ -588,6 +635,7 @@
                                         {/each}
                                     </div>
                                 {/each}
+                            {/if}
                         {/await}
                     </div>
                 </div>
@@ -620,6 +668,7 @@
 </div>
 
 <style>
+
     progress {
         /* Reset the default appearance */
         -webkit-appearance: none;
@@ -643,6 +692,13 @@
     }
     .this-prog-container {
         border:none;
+    }
+
+    ::-webkit-search-cancel-button,
+    ::-webkit-search-decoration,
+    ::-webkit-search-results-decoration,
+    ::-webkit-search-results-decoration {
+        all:unset
     }
 
     .search-input {
@@ -896,5 +952,236 @@
         width:100%;
         min-width: 70px
     }
+
+    .settings:hover,
+    input[type=number]:hover,
+    .desired-button:hover,
+    .step-button:hover,
+    .dropbtn:hover,
+    input[type=checkbox]:hover,
+    .search-input:hover {
+        outline: #c5f17e solid 3px;
+        outline-offset: -1px;
+    }
+
+    @keyframes animateFrames {
+    0%    { opacity: 0; }
+    96%    { opacity: 0;  }
+    99%    { opacity: 1; } 
+    100%  { opacity: 0; }
+    }
+      @keyframes fade {
+    0%    { opacity: 0; }
+    100%    { opacity: 1; }
+
+    }
+    .loading {
+        animation: fade ease-in-out 1s infinite alternate;
+    }
+
+    .loading-img {
+        opacity: 0;
+        position:absolute;
+        height:128px;
+        width:192px;
+        filter:brightness(1.1);
+        
+    }
+    #loading-frame-1 {
+        animation: animateFrames 4800ms steps(2, jump-none) 0s infinite reverse;
+    }
+    #loading-frame-2 {
+        animation: animateFrames 4800ms steps(2, jump-none) 75ms infinite reverse;
+    }
+    #loading-frame-3 {
+        animation: animateFrames 4800ms steps(2, jump-none) 150ms infinite reverse;
+    }
+    #loading-frame-4 {
+        animation: animateFrames 4800ms steps(2, jump-none) 225ms infinite reverse;
+    }
+    #loading-frame-5 {
+        animation: animateFrames 4800ms steps(2, jump-none) 300ms infinite reverse;
+    }
+    #loading-frame-6 {
+        animation: animateFrames 4800ms steps(2, jump-none) 375ms infinite reverse;
+    }
+    #loading-frame-7 {
+        animation: animateFrames 4800ms steps(2, jump-none) 450ms infinite reverse;
+    }
+    #loading-frame-8 {
+        animation: animateFrames 4800ms steps(2, jump-none) 525ms infinite reverse;
+    }
+    #loading-frame-9 {
+        animation: animateFrames 4800ms steps(2, jump-none) 600ms infinite reverse;
+    }
+    #loading-frame-10 {
+        animation: animateFrames 4800ms steps(2, jump-none) 675ms infinite reverse;
+    }
+    #loading-frame-11 {
+        animation: animateFrames 4800ms steps(2, jump-none) 750ms infinite reverse;
+    }
+    #loading-frame-12 {
+        animation: animateFrames 4800ms steps(2, jump-none) 825ms infinite reverse;
+    }
+    #loading-frame-13 {
+        animation: animateFrames 4800ms steps(2, jump-none) 900ms infinite reverse;
+    }
+    #loading-frame-14 {
+        animation: animateFrames 4800ms steps(2, jump-none) 975ms infinite reverse;
+    }
+    #loading-frame-15 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1050ms infinite reverse;
+    }
+    #loading-frame-16 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1125ms infinite reverse;
+    }
+    #loading-frame-17 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1200ms infinite reverse;
+    }
+    #loading-frame-18 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1275ms infinite reverse;
+    }
+    #loading-frame-19 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1350ms infinite reverse;
+    }
+    #loading-frame-20 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1425ms infinite reverse;
+    }
+    #loading-frame-21 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1500ms infinite reverse;
+    }
+    #loading-frame-22 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1575ms infinite reverse;
+    }
+    #loading-frame-23 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1650ms infinite reverse;
+    }
+    #loading-frame-24 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1725ms infinite reverse;
+    }
+    #loading-frame-25 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1800ms infinite reverse;
+    }
+    #loading-frame-26 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1875ms infinite reverse;
+    }
+    #loading-frame-27 {
+        animation: animateFrames 4800ms steps(2, jump-none) 1950ms infinite reverse;
+    }
+    #loading-frame-28 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2025ms infinite reverse;
+    }
+    #loading-frame-29 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2100ms infinite reverse;
+    }
+    #loading-frame-30 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2175ms infinite reverse;
+    }
+    #loading-frame-31 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2250ms infinite reverse;
+    }
+    #loading-frame-32 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2325ms infinite reverse;
+    }
+    #loading-frame-33 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2400ms infinite reverse;
+    }
+    #loading-frame-34 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2475ms infinite reverse;
+    }
+    #loading-frame-35 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2550ms infinite reverse;
+    }
+    #loading-frame-36 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2625ms infinite reverse;
+    }
+    #loading-frame-37 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2700ms infinite reverse;
+    }
+    #loading-frame-38 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2775ms infinite reverse;
+    }
+    #loading-frame-39 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2850ms infinite reverse;
+    }
+    #loading-frame-40 {
+        animation: animateFrames 4800ms steps(2, jump-none) 2925ms infinite reverse;
+    }
+    #loading-frame-41 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3000ms infinite reverse;
+    }
+    #loading-frame-42 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3075ms infinite reverse;
+    }
+    #loading-frame-43 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3150ms infinite reverse;
+    }
+    #loading-frame-44 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3225ms infinite reverse;
+    }
+    #loading-frame-45 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3300ms infinite reverse;
+    }
+    #loading-frame-46 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3375ms infinite reverse;
+    }
+    #loading-frame-47 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3450ms infinite reverse;
+    }
+    #loading-frame-48 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3525ms infinite reverse;
+    }
+    #loading-frame-49 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3600ms infinite reverse;
+    }
+    #loading-frame-50 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3675ms infinite reverse;
+    }
+    #loading-frame-51 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3750ms infinite reverse;
+    }
+    #loading-frame-52 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3825ms infinite reverse;
+    }
+    #loading-frame-53 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3900ms infinite reverse;
+    }
+    #loading-frame-54 {
+        animation: animateFrames 4800ms steps(2, jump-none) 3975ms infinite reverse;
+    }
+    #loading-frame-55 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4050ms infinite reverse;
+    }
+    #loading-frame-56 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4125ms infinite reverse;
+    }
+    #loading-frame-57 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4200ms infinite reverse;
+    }
+    #loading-frame-58 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4275ms infinite reverse;
+    }
+    #loading-frame-59 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4350ms infinite reverse;
+    }
+    #loading-frame-60 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4425ms infinite reverse;
+    }
+    #loading-frame-61 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4500ms infinite reverse;
+    }
+    #loading-frame-62 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4575ms infinite reverse;
+    }
+    #loading-frame-63 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4650ms infinite reverse;
+    }
+    #loading-frame-64 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4725ms infinite reverse;
+    }
+    #loading-frame-65 {
+        animation: animateFrames 4800ms steps(2, jump-none) 4800ms infinite reverse;
+    }
+
 
 </style>
